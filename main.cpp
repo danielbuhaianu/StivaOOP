086612815f247294f41d48b13ef813ffd79b14dd
Daniel Buhăianu <danielbuhaianu99@gmail.com>
2017-04-25 14:10:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+ifstream fin ("STIVA.IN");
+ofstream fout ("STIVA.OUT");
+
+struct nod
+{
+    nod*urm;
+    int info;
+};
+
+class Stiva
+{
+    private:
+        nod*prim;
+
+    public:
+        Stiva()
+        {
+            prim=NULL;
+        }
+        Stiva(Stiva&a);
+        ~Stiva();
+        void creareStiva();
+        void afisareStiva();
+        nod* extrageNod(nod*&p);
+        void adaugaNod(int info);
+        void conversieBaza10();
+        Stiva operator+(Stiva&a);
+        Stiva operator-(Stiva&a);
+        Stiva operator*(int cifra);
+        bool operator>(Stiva&a);
+        Stiva operator*(Stiva&a);
+};
+
+Stiva::Stiva(Stiva&a)
+{
+    nod*p,*q,*u;
+    p=a.prim;
+    prim=new nod;
+    prim->info = p->info;
+    p=p->urm;
+    u=prim;
+    while(p!=NULL)
+    {
+        q=new nod;
+        q->info = p->info;
+        u->urm = q;
+        u=q;
+        p=p->urm;
+    }
+    u->urm=NULL;
+}
+Stiva::~Stiva()
+{
+    nod*p=prim,*q;
+    while(p!=NULL)
+    {
+        q=p;
+        p=p->urm;
+        delete q;
+    }
+    prim=NULL;
+}
+void Stiva::adaugaNod(int info)
+{
+    if(prim==NULL)
+    {
+        prim=new nod;
+        prim->info = info;
+        prim->urm=NULL;
+    }
+    else
+    {
+        nod*q;
+        q=new nod;
+        q->info=info;
+        q->urm=prim;
+        prim=q;
+    }
+
+}
+void Stiva::creareStiva()
+{
+    int b,cif;
+    fin>>b;
+    while(fin>>cif)
+        adaugaNod(cif);
+}
+void Stiva::afisareStiva()
+{
+    if(prim==NULL)
+        fout<<"Nu sunt elemente in stiva. \n";
+    else
+    {
+        fout<<"Stiva contine elementele: ";
+        for(nod*p=prim;p!=NULL;p=p->urm)
+        {
+            fout<<p->info<<' ';
+        }
+        fout<<'\n';
+    }
+}
+nod* Stiva::extrageNod(nod*&p)
+{
+    if(prim!=NULL)
+    {
+        p=prim;
+        prim=prim->urm;
+        return p;
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    Stiva a;
+    nod*p;
+    a.creareStiva();
+    a.afisareStiva();
+    p=a.extrageNod(p);
+    if(p!=NULL)
+    fout<<p->info<<'\n';
+    else
+        fout<<"Nod NULL"<<'\n';
+    a.afisareStiva();
+    return 0;
+}
